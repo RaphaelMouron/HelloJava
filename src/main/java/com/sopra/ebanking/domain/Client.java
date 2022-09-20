5888fd82ef641fc3ac7f39e5ed6d38d13e6d1884
mouron <raphael.mouron@soprasteria.com>
2022-09-20 15:52:47 +0200

    suppression erreur

--- src/main/java/com/sopra/ebanking/domain/Client.java
+++ src/main/java/com/sopra/ebanking/domain/Client.java
@@ -3,7 +3,7 @@ package com.sopra.ebanking.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id
+import javax.persistence.Id;
 
 /**
  * 
