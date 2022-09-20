57e140dcda1a136adfddf7bc037e3e5f87808d5b
mouron <raphael.mouron@soprasteria.com>
2022-09-20 10:15:35 +0200

    ajout variable

--- src/main/java/com/sopra/ebanking/EbankingApplication.java
+++ src/main/java/com/sopra/ebanking/EbankingApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EbankingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EbankingApplication.class, args);
+		SpringApplication.run(EbankingApplication.class, args)
+		age=22;
 	}
 
 }
